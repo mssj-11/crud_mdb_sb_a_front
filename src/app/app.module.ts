d68128a2deeaca86cf530285ce9837a884f485d4
Mike <80673026+mssj-11@users.noreply.github.com>
2023-07-27 23:07:29 -0600

    Components , Service 6 Class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-//  Validations with sweetalert2 & Animations with ngx-toastr
+//  Libraries
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+//  Components
+import { ListComponent } from './product/list.component';
+import { DetailComponent } from './product/detail.component';
+import { CreateComponent } from './product/create.component';
+import { UpdateComponent } from './product/update.component';
+
+//  External: Validations with sweetalert2 & Animations with ngx-toastr
 import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
@@ -12,14 +22,20 @@ import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    DetailComponent,
+    CreateComponent,
+    UpdateComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     SweetAlert2Module.forRoot(),
     BrowserAnimationsModule,
-    ToastrModule.forRoot()
+    ToastrModule.forRoot(),
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
