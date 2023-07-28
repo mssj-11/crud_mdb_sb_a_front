05c7163732173d283ceae75d958ceeec4f0583ad
Mike <80673026+mssj-11@users.noreply.github.com>
2023-07-27 23:15:11 -0600

    Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListComponent } from './product/list.component';
+import { DetailComponent } from './product/detail.component';
+import { CreateComponent } from './product/create.component';
+import { UpdateComponent } from './product/update.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ListComponent},
+  {path: 'detail', component: DetailComponent},
+  {path: 'create', component: CreateComponent},
+  {path: 'update', component: UpdateComponent},
+  {path: '**', redirectTo: '', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
