67cf3e32fb21c87b0272d868c45b9595512976b0
Mike <80673026+mssj-11@users.noreply.github.com>
2023-07-29 21:01:20 -0600

    Product Model

--- src/app/model/product.ts
+++ src/app/model/product.ts
@@ -1,2 +1,10 @@
 export class Product {
+    id: number; //In case of error change to: id!: number;
+    name: string;
+    price: number;
+
+    constructor(name: string, price: number){
+        this.name = name;
+        this.price = price;
+    }
 }
